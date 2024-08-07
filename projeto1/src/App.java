f18cee688d54b08f3706dc7a7d9c3b3a3b169823
Sofia Sartori <sofia.sartori24@gmail.com>
2024-08-07 23:22:47 +0000

    exercicio 1

--- projeto1/src/App.java
+++ projeto1/src/App.java
@@ -1,5 +1,7 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+public class App { 
+    public static void main(String[] args) throws Exception { 
+        System.out.println("\nHi!"); 
+        String version = System.getProperty("java.version"); 
+        System.out.println("Running Java Version "+version+"\n"); 
+    } 
 }
